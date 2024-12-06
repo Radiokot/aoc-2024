54d4b70336a3278f2f15d2fe33689033205b7ffb
Oleg Koretsky <oleg@radiokot.com.ua>
2024-12-06 16:28:50 +0200

    Solve day 6 (bruteforce part 2)

---
+++ src/Day06.kt
@@ -0,0 +1,122 @@
+enum class Direction(val incX: Int, val incY: Int, val marker: Char) {
+    N(incX = 0, incY = -1, marker = '^'),
+    E(incX = 1, incY = 0, marker = '>'),
+    S(incX = 0, incY = 1, marker = 'v'),
+    W(incX = -1, incY = 0, marker = '<'),
+    ;
+
+    fun turnClockwise() =
+        values()[(this.ordinal + 1) % values().size]
+}
+
+@JvmInline
+value class Position(private val pair: Pair<Int, Int>) {
+    constructor(x: Int, y: Int) : this(x to y)
+
+    val x: Int
+        get() = pair.first
+
+    val y: Int
+        get() = pair.second
+
+    fun step(direction: Direction) =
+        Position(
+            x = x + direction.incX,
+            y = y + direction.incY,
+        )
+}
+
+class GoingInCirclesException : Exception()
+
+typealias VisitMap = MutableMap<Position, MutableSet<Direction>>
+
+fun patrol(
+    field: List<CharArray>,
+    startPosition: Position,
+    startDirection: Direction,
+): VisitMap {
+    var position = startPosition
+    var direction = startDirection
+    val visits: MutableMap<Position, MutableSet<Direction>> = mutableMapOf()
+
+    while (true) {
+        if (visits[position]?.contains(direction) == true) {
+            throw GoingInCirclesException()
+        }
+
+        visits.getOrPut(position, ::mutableSetOf).add(direction)
+
+        val nextStepPosition = position.step(direction)
+        val nextStepTile = runCatching { field[nextStepPosition.y][nextStepPosition.x] }.getOrNull()
+
+        when (nextStepTile) {
+            '#', 'O' -> {
+                direction = direction.turnClockwise()
+            }
+
+            null -> {
+                break
+            }
+
+            else -> {
+                position = nextStepPosition
+            }
+        }
+    }
+
+    return visits
+}
+
+fun main() {
+    val input =
+        readInput(day = 6)
+//        """
+//            ....#.....
+//            .........#
+//            ..........
+//            ..#.......
+//            .......#..
+//            ..........
+//            .#..^.....
+//            ........#.
+//            #.........
+//            ......#...
+//        """.trimIndent().lines()
+            .map(String::toCharArray)
+
+    val startPosition = input
+        .indexOfFirst { it.contains('^') }
+        .let { y -> input[y].indexOf('^') to y }
+        .let(::Position)
+
+
+    // Part 1.
+    val visits = patrol(
+        field = input,
+        startPosition = startPosition,
+        startDirection = Direction.N,
+    )
+    println(visits.size)
+
+    // Part 2.
+    val possibleObstacles = mutableSetOf<Position>()
+    input.forEachIndexed { y, line ->
+        line.forEachIndexed { x, char ->
+            if (char == '.') {
+                try {
+                    line[x] = 'O'
+                    patrol(
+                        field = input,
+                        startPosition = startPosition,
+                        startDirection = Direction.N,
+                    )
+                } catch (target: GoingInCirclesException) {
+                    possibleObstacles.add(Position(x, y))
+                } finally {
+                    line[x] = char
+                }
+            }
+        }
+    }
+    println(possibleObstacles.size)
+}
