55b8ce924e9d15b73b28d6a4e67f0340b526d0ce
Oleg Koretsky <oleg@radiokot.com.ua>
2024-12-20 20:18:41 +0200

    Speed up day 20

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,32 +1,13 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.runBlocking
-
-fun main() = runBlocking(Dispatchers.Default) {
+fun main() {
     val START = 'S'
     val FINISH = 'E'
     val WALL = '#'
 
-    // 20 takes around 40 seconds, 2 – just around 2.
-    val maxCheatDistance = 2
+    val maxCheatDistance = 20
     val benefitThreshold = 100
     val map = readInput(day = 20, test = false)
         .filterNot(String::isEmpty)
         .map(String::toMutableList)
-
-    fun printMap(path: Set<Position>? = null) {
-        map.forEachIndexed { y, row ->
-            println(
-                row.mapIndexed { x, cell ->
-                    if (path != null && Position(x, y) in path)
-                        '◆'
-                    else
-                        cell
-                }.joinToString("")
-            )
-        }
-    }
 
     val startPosition = map
         .first { it.contains(START) }
@@ -37,31 +18,28 @@ fun main() = runBlocking(Dispatchers.Default) {
             )
         }
 
-    val originalPath = buildList<Position> {
+    val originalPathIndices = mutableMapOf<Position, Int>(startPosition to 0)
+    buildList<Position> {
         add(startPosition)
-
-        var previous = startPosition
         while (map[last()] != FINISH) {
-            val last = last()
-            Direction.values()
-                .map(last::move)
-                .filter { it != last && it != previous && map[it] != WALL }
-                .also(::addAll)
-            previous = last
+            val next = Direction.values()
+                .map(last()::move)
+                .first { !originalPathIndices.contains(it) && map[it] != WALL }
+            add(next)
+            originalPathIndices[next] = lastIndex
         }
     }
-    val originalPathSet = originalPath.toSet()
 
     fun findCheats(from: Position): Map<Int, MutableSet<String>> {
-        val fromIndex = originalPath.indexOf(from)
+        val fromIndex = originalPathIndices.getValue(from)
         val result = mutableMapOf<Int, MutableSet<String>>()
 
         for (x in (from.x - maxCheatDistance..from.x + maxCheatDistance)) {
             for (y in (from.y - maxCheatDistance..from.y + maxCheatDistance)) {
                 val current = Position(x, y)
                 val distance = current.distanceTo(from)
-                if (current in originalPathSet && distance <= maxCheatDistance) {
-                    val benefit = originalPath.indexOf(current) - fromIndex - distance
+                if (originalPathIndices.contains(current) && distance <= maxCheatDistance) {
+                    val benefit = originalPathIndices.getValue(current) - fromIndex - distance
                     if (benefit > 0) {
                         result.getOrPut(benefit, ::mutableSetOf) += "$from$current"
                     }
@@ -72,11 +50,8 @@ fun main() = runBlocking(Dispatchers.Default) {
         return result
     }
 
-    val cheatsByBenefit = originalPath
-        .map { pathPosition ->
-            async { findCheats(pathPosition) }
-        }
-        .awaitAll()
+    val cheatsByBenefit = originalPathIndices.keys
+        .map(::findCheats)
         .fold(mutableMapOf<Int, Int>()) { result, cheatsAtPosition ->
             cheatsAtPosition.forEach { (benefit, cheats) ->
                 result[benefit] = (result[benefit] ?: 0) + cheats.size
